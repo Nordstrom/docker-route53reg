276785f24c39aa00e3bc0d0e02e811b275d501cf
Emmanuel Gomez <emmanuel@gomez.io>
2016-01-05 15:19:25 -0800

    Pass a fully-qualified domain name. Rename a few variables.

--- conf/prepareRoute.sh
+++ conf/prepareRoute.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-IP=`ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{print $1}'`
-INT_IP="${IP_ADDRESS:-$IP}"
-INT_STACK="${STACK_NAME:?"must be set"}"
-INT_ZONEID="${ROUTE53_ZONE_ID:?"must be set"}"
+eth0_ip=`ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{print $1}'`
+target_ip="${IP_ADDRESS:-$eth0_ip}"
+domain_name="${DOMAIN_NAME:?"must be set"}"
+zone_id="${ROUTE53_ZONE_ID:?"must be set"}"
 
-sed -r -e "s/;stack_name;/$INT_STACK/g" \
-		-e "s/;private_ipv4;/$INT_IP/g" \
-		 /templates/route.json.tmpl > /templates/route.json
+sed -e "s/;domain_name;/${domain_name}/g" \
+    -e "s/;target_ip;/${target_ip}/g" \
+    /templates/route.json.tmpl > /templates/route.json
 
 /usr/local/bin/aws route53 change-resource-record-sets \
-    --hosted-zone-id ${INT_ZONEID} \
+    --hosted-zone-id ${zone_id} \
     --change-batch file:///templates/route.json
