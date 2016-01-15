89161051d4535891bc670360e825c2bfcb852e90
Emmanuel Gomez <emmanuel@gomez.io>
2016-01-15 11:37:23 -0800

    Copy/paste oops; don’t need this layer of escaping outside of make.

--- prepareRoute.sh
+++ prepareRoute.sh
@@ -5,8 +5,8 @@ target_ip="${IP_ADDRESS:-$eth0_ip}"
 domain_name="${DOMAIN_NAME:?"must be set"}"
 zone_id="${ROUTE53_ZONE_ID:?"must be set"}"
 
-sed -e "s|\$${DOMAIN_NAME}|${domain_name}|g" \
-    -e "s|\$${TARGET_IP}|${target_ip}|g" \
+sed -e "s|\${DOMAIN_NAME}|${domain_name}|g" \
+    -e "s|\${TARGET_IP}|${target_ip}|g" \
     /templates/route.json.tmpl > /route.json
 
 /usr/local/bin/aws route53 change-resource-record-sets \
